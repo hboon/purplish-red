e5d8104bf4b25bce63ec1c7057063e63cd0145f9
Hwee-Boon Yar <hboon@motionobj.com>
2014-01-15 17:26:27 +0800

    Add convenience methods for CGRect

--- lib/purplish-red/non-ui/ns_array.rb
+++ lib/purplish-red/non-ui/ns_array.rb
@@ -22,5 +22,66 @@ class CGRect
     self.size = [size.width*num, size.height*num]
     self
   end
+
+
+  def left
+    origin.x
+  end
+
+
+  def left=(x)
+    origin.x = x
+  end
+
+
+  def top
+    origin.y
+  end
+
+
+  def top=(y)
+    origin.y = y
+  end
+
+
+  def right
+    origin.x+size.width
+  end
+
+
+
+  def right=(right)
+    origin.x = right - size.width
+  end
+
+
+  def bottom
+    origin.y+size.height
+  end
+
+
+  def bottom=(bottom)
+    origin.y = bottom - size.height
+  end
+
+
+  def center_x
+    left+size.width/2
+  end
+
+
+  def center_x=(center_x)
+    self.left = center_x-size.width/2
+  end
+
+
+  def center_y
+    top+size.height/2
+  end
+
+
+  def center_y=(center_y)
+    self.top = center_y-size.height/2
+  end
 end
 
