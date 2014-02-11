4b50906e3707e966b17988dd9caa6040a0ed9ec9
Hwee-Boon Yar <hboon@motionobj.com>
2014-02-11 17:24:49 +0800

    Add #if_not_nil: and #if_nil:

---
+++ lib/purplish-red/non-ui/smalltalk.rb
@@ -0,0 +1,20 @@
+class Object
+  def if_not_nil(&block)
+    block.call(self)
+  end
+
+
+  def if_nil(&block)
+    #do nothing
+  end
+end
+class NilClass
+  def if_not_nil(&block)
+    #do nothing
+  end
+
+
+  def if_nil(&block)
+    block.call
+  end
+end
