b0533d210752d496d86d7dbad8bccbd67918d6e5
Hwee-Boon Yar <hboon@motionobj.com>
2014-07-19 20:43:18 +0800

    Fix: make things work in OS X (at least be able to load)

--- lib/purplish-red/non-ui/state.rb
+++ lib/purplish-red/non-ui/state.rb
@@ -1,9 +1,11 @@
 if Object.const_defined?('PurplishRed')
-  NSNotificationCenter.defaultCenter.addObserverForName(UIApplicationDidFinishLaunchingNotification, object:nil, queue:nil, usingBlock:lambda do |notification|
-    PurplishRed.app_resumed = false
-  end)
+  if PurplishRed::ios?
+    NSNotificationCenter.defaultCenter.addObserverForName(UIApplicationDidFinishLaunchingNotification, object:nil, queue:nil, usingBlock:lambda do |notification|
+      PurplishRed.app_resumed = false
+    end)
 
-  NSNotificationCenter.defaultCenter.addObserverForName(UIApplicationWillEnterForegroundNotification, object:nil, queue:nil, usingBlock:lambda do |notification|
-    PurplishRed.app_resumed = true
-  end)
+    NSNotificationCenter.defaultCenter.addObserverForName(UIApplicationWillEnterForegroundNotification, object:nil, queue:nil, usingBlock:lambda do |notification|
+      PurplishRed.app_resumed = true
+    end)
+  end
 end
--- lib/purplish-red/purplish_red.rb
+++ lib/purplish-red/purplish_red.rb
@@ -6,4 +6,14 @@ module PurplishRed
   def self.app_resumed?
     @app_resumed
   end
+
+  module_function
+
+  def osx?
+    Kernel.const_defined?(:NSApplication)
+  end
+
+  def ios?
+    Kernel.const_defined?(:UIApplication)
+  end
 end
