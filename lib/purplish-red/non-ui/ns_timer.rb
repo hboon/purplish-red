c6a4034997ba318587b0383abfd7286f2b763a77
Hwee-Boon Yar <hboon@motionobj.com>
2015-02-01 19:03:46 +0800

    Should not check against BubbleWrap

--- lib/purplish-red/non-ui/ns_timer.rb
+++ lib/purplish-red/non-ui/ns_timer.rb
@@ -1,6 +1,6 @@
 class NSTimer
   def self.timer_after(seconds, &block)
-    block.weak! if BubbleWrap.use_weak_callbacks?
+    block.weak!
     NSTimer.scheduledTimerWithTimeInterval(seconds, target:self, selector:'timer_fired_with_block_as_user_info:', userInfo:block, repeats:false)
   end
 
