2a8078a19ab96b875452e4ea5c4959234bdfedf5
Hwee-Boon Yar <hboon@motionobj.com>
2015-04-27 22:16:10 +0800

    Support rounded corners with ViewWithBackgroundColor

--- lib/purplish-red/ui/osx/ns_view_with_background_color.rb
+++ lib/purplish-red/ui/osx/ns_view_with_background_color.rb
@@ -1,9 +1,14 @@
 class ViewWithBackgroundColor < NSView
-  attr_accessor :background_color
+  attr_accessor :background_color, :background_radius
 
   def drawRect(dirtyRect)
     return unless background_color
     background_color.set
-    NSRectFill(bounds)
+    if background_radius && background_radius > 0
+      path = NSBezierPath.bezierPathWithRoundedRect(frame, xRadius:background_radius, yRadius:background_radius)
+      path.fill
+    else
+      NSRectFill(bounds)
+    end
   end
 end
