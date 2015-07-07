3c62a1f3f88bfdd51dad10c2f9a0290811b5d5e1
Hwee-Boon Yar <hboon@motionobj.com>
2015-07-07 19:58:25 +0800

    Add to NSMenu

--- lib/purplish-red/ui/osx/ns_menu.rb
+++ lib/purplish-red/ui/osx/ns_menu.rb
@@ -6,4 +6,8 @@ class NSMenu
   def index_with_title(aString)
     (0...numberOfItems).detect {|i| itemAtIndex(i).title == aString}
   end
+
+  def index_with_action(aStringOrSymbol)
+    (0...numberOfItems).detect {|i| itemAtIndex(i).action == aStringOrSymbol.to_sym}
+  end
 end
