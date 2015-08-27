5b63ab2bb237c392e07fd13dc5dc83225bc9e078
Hwee-Boon Yar <hboon@motionobj.com>
2015-08-28 01:33:06 +0800

    Add convenience functions for archiving and unarching using NSCoder

--- lib/purplish-red/non-ui/ns_object.rb
+++ lib/purplish-red/non-ui/ns_object.rb
@@ -1,5 +1,13 @@
 class NSObject
+  def self.unarchive_from(aPath)
+    NSKeyedUnarchiver.unarchiveObjectWithFile(aPath)
+  end
+
   def to_archive_data
     NSKeyedArchiver.archivedDataWithRootObject(self)
+  end
+
+  def archive_to(aPath)
+    NSKeyedArchiver.archiveRootObject(self, toFile:aPath)
   end
 end
