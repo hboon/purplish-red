87e848be9ac8c724fdbbb8b0ae634164fd5c0f71
Hwee-Boon Yar <hboon@motionobj.com>
2016-01-08 21:25:54 +0800

    Move MessageEatingNil to module

--- lib/purplish-red/non-ui/message_eating_nil.rb
+++ lib/purplish-red/non-ui/message_eating_nil.rb
@@ -1,32 +1,34 @@
-class MessageEatingNil
-  def self.log_method_missing=(aBool)
-    @log_method_missing = aBool
-  end
+module PurplishRed
+  class MessageEatingNil
+    def self.log_method_missing=(aBool)
+      @log_method_missing = aBool
+    end
 
-  def self.log_method_missing?
-    !!@log_method_missing
-  end
+    def self.log_method_missing?
+      !!@log_method_missing
+    end
 
-  #Block with args (method_name, *args)
-  def self.on_method_missing(&block)
-    self.log_method_missing = true
-    @method_missing_block = block
-  end
+    #Block with args (method_name, *args)
+    def self.on_method_missing(&block)
+      self.log_method_missing = true
+      @method_missing_block = block
+    end
 
-  def self.method_missing_block
-    @method_missing_block
-  end
+    def self.method_missing_block
+      @method_missing_block
+    end
 
-  def self.nil
-    Dispatch.once { @instance ||= new }
-    @instance
-  end
+    def self.nil
+      Dispatch.once { @instance ||= new }
+      @instance
+    end
 
-  def method_missing(m, *args, &block)
-    if self.class.log_method_missing?
-      self.class.method_missing_block.call(m, *args)
-    else
-      self
+    def method_missing(m, *args, &block)
+      if self.class.log_method_missing?
+        self.class.method_missing_block.call(m, *args)
+      else
+        self
+      end
     end
   end
 end
