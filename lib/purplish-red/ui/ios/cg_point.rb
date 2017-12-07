0f7dba82e470b2005878fd5f3e2b562c690d1aac
Hwee-Boon Yar <hboon@motionobj.com>
2017-12-07 23:16:53 +0800

    Add #translate for CGPoint and CGRect

--- lib/purplish-red/ui/ios/cg_point.rb
+++ lib/purplish-red/ui/ios/cg_point.rb
@@ -2,4 +2,8 @@ class CGPoint
   def inspect
     "#<#{self.class.name} [#{x}, #{y}]>"
   end
+
+  def translate(point)
+    CGPointMake(x + point.x, y + point.y)
+  end
 end
--- lib/purplish-red/ui/ios/cg_rect.rb
+++ lib/purplish-red/ui/ios/cg_rect.rb
@@ -2,4 +2,9 @@ class CGRect
   def inspect
     "#<#{self.class.name} [[#{origin.x}, #{origin.y}], [#{size.width}, #{size.height}]]>"
   end
+
+  def translate(point)
+    pt = origin.translate(point)
+    CGRectMake(pt.x, pt.y, size.width, size.height)
+  end
 end
