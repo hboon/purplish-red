35d3a08c1ddbbdedaefe94619ad868e7ba0dfe88
Hwee-Boon Yar <hboon@motionobj.com>
2017-12-29 21:50:25 +0800

    Returns the list of names of methods generated by bool_attr_accessor, so they can be made private separately

--- lib/purplish-red/non-ui/bool_attr_accessor.rb
+++ lib/purplish-red/non-ui/bool_attr_accessor.rb
@@ -1,13 +1,20 @@
 class Class
+  # Returns the list of method names (symbols) generated. Useful for making them private
   def bool_attr_accessor(*my_accessors)
+    method_names = []
     my_accessors.each do |accessor|
-      define_method(("#{accessor}?").to_sym) do
+      name = ("#{accessor}?").to_sym
+      method_names << name
+      define_method(name) do
         !!instance_variable_get("@#{accessor}") #rubocop:disable DoubleNegation
       end
 
-      define_method("#{accessor}=") do |accessor_value|
+      name = "#{accessor}=".to_sym
+      method_names << name
+      define_method(name) do |accessor_value|
         instance_variable_set("@#{accessor}", accessor_value)
       end
     end
+    method_names
   end
 end
